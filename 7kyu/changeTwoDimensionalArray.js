5c6e5a30e741c40bb340e9b70b96c256a3f920ca
cc <164414627+m-cal@users.noreply.github.com>
2024-04-06 08:59:26 -0400

    Solve "Change Two Dimensional Array"

--- 7kyu/changeTwoDimensionalArray.js
+++ 7kyu/changeTwoDimensionalArray.js
@@ -1,9 +1,7 @@
 function matrix(array) {
   let arr = array;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i][i] < 0) {
-      arr[i][i] = 0;
-    }
+    arr[i][i] = arr[i][i] < 0 ? 0 : 1;
   }
   return arr;
 }
