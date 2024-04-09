252edbe1ccb72c7bc848dee4357a0be4eb5cc4ab
cc <164414627+m-cal@users.noreply.github.com>
2024-04-09 10:42:38 -0400

    Solve "Mumbling"

--- 7kyu/mumbling.js
+++ 7kyu/mumbling.js
@@ -1,16 +1,7 @@
 function accum(str) {
-  let newStr = '';
+  let arr = [];
   for (let i = 0; i < str.length; i++) {
-    console.log(i);
-    console.log(str)
-    console.log(str[i])
-    newStr.concat(str[i].repeat(i + 1));
-    console.log(newStr)
+    arr.push(str.charAt(i).toUpperCase() + str.charAt(i).toLowerCase().repeat(i));)
   }
-  return newStr;
+  return arr.join('-');
 }
-
-
-// Can definitely solve with the reduce method, 
-// but am not sure how it's possible for the above code to return an empty string
-// so will more thoroughly investigation until I have my answer
