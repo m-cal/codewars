cc9597f6c0345865786802977bcfa693ab9c2923
cc <164414627+m-cal@users.noreply.github.com>
2024-04-09 10:44:12 -0400

    Fix typo causing syntax error

--- 7kyu/mumbling.js
+++ 7kyu/mumbling.js
@@ -1,7 +1,7 @@
 function accum(str) {
   let arr = [];
   for (let i = 0; i < str.length; i++) {
-    arr.push(str.charAt(i).toUpperCase() + str.charAt(i).toLowerCase().repeat(i));)
+    arr.push(str.charAt(i).toUpperCase() + str.charAt(i).toLowerCase().repeat(i));
   }
   return arr.join('-');
 }
