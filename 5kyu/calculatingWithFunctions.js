7d2767e0ccddea9cea038be6bd0a5e4a9ab68565
cc <164414627+m-cal@users.noreply.github.com>
2024-04-09 14:49:28 -0400

    Update solution

--- 5kyu/calculatingWithFunctions.js
+++ 5kyu/calculatingWithFunctions.js
@@ -1,38 +1,113 @@
+function zero(op = undefined) {
+  let base = 0;
+  if (!op) {
+    return 0;
+  } else if (op) {
+    return op(0);
+  }
+}
+
+function one(op = undefined) {
+  let base = 1;
+  if (!op) {
+    return 1;
+  } else if (op) {
+    return op(1);
+  }
+}
+
+function two(op = undefined) {
+  let base = 2;
+  if (!op) {
+    return 2;
+  } else if (op) {
+    return op(2);
+  }
+}
+
+function three(op = undefined) {
+  let base = 3;
+  if (!op) {
+    return 3;
+  } else if (op) {
+    return op(3);
+  }
+}
+
+function four(op = undefined) {
+  let base = 4;
+  if (!op) {
+    return 4;
+  } else if (op) {
+    return op(4);
+  }
+}
+
+function five(op = undefined) {
+  let base = 5;
+  if (!op) {
+    return 5;
+  } else if (op) {
+    return op(5);
+  }
+}
+
+function six(op = undefined) {
+  let base = 6;
+  if (!op) {
+    return 6;
+  } else if (op) {
+    return op(6);
+  }
+}
+
 function seven(op = undefined) {
   let base = 7;
   if (!op) {
     return 7;
   } else if (op) {
-    op();
+    return op(7);
   }
 }
 
-function plus(num, base = base) {
-  return base + num;
+function eight(op = undefined) {
+  let base = 8;
+  if (!op) {
+    return 8;
+  } else if (op) {
+    return op(8);
+  }
 }
 
-function minus() {}
-function times() {}
-function dividedBy() {}
+function nine(op = undefined) {
+  let base = 9;
+  if (!op) {
+    return 9;
+  } else if (op) {
+    return op(9);
+  }
+}
 
+function plus(num) {
+  return function (a) {
+    return a + num;
+  }
+}
 
-/*
+function minus(num) {
+  return function (a) {
+    return a - num;
+  }
+}
 
-seven(plus(seven()));
+function times(num) {
+  return function (a) {
+    return a * num;
+  }
+}
 
-*/
-
-
-
-
-
-
-
-/*
-
-seven(times(five())); // must return 35
-four(plus(nine())); // must return 13
-eight(minus(three())); // must return 5
-six(dividedBy(two())); // must return 3
-
-*/
+function dividedBy(num) {
+  return function (a) {
+    return Math.floor(a / num);
+  }
+}
